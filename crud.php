cc9a8cdade4591dce99ef0f2d16385948f741fb6
davechris1218 <74490003+davechris1218@users.noreply.github.com>
2021-04-30 23:31:04 +0700

    native login crud

--- crud.php
+++ crud.php
@@ -148,7 +148,10 @@ if (isset($_GET['logout'])) {
                                             <tr>
                                                 <td><?php echo $x; ?></td>
                                                 <td><?php echo  $r['item_name']; ?></td>
+                                                <td><?php echo  $r['item_type']; ?></td>
                                                 <td><?php echo  $r['description']; ?></td>
+                                                <td><?php echo  $r['item_price']; ?></td>
+                                                <td><?php echo  $r['item_image']; ?></td>
                                                 <td>
                                                     <a href="javascript:void(0)" class='open_modal' id='<?php echo  $r['id']; ?>'>Edit</a>
                                                     <a href="javascript:void(0)" class="delete_modal" data-id='<?php echo  $r['id']; ?>'>Delete</a>
@@ -199,7 +202,7 @@ if (isset($_GET['logout'])) {
                                                 <div class="form-group" style="padding-bottom: 20px;">
                                                     <label for="Price">Price</label>
                                                     <input type="text" name="item_price" id="item-price" class="form-control" placeholder="Price" required />
-                                                </div>item_priceitem_priceitem_priceitem_price
+                                                </div>
 
                                                 <div class="form-group" style="padding-bottom: 20px;">
                                                     <label for="Image">Image</label>
