c87f47b428ec07528a6dc5256d19cbcf3bebad18
davechris1218 <74490003+davechris1218@users.noreply.github.com>
2021-05-15 22:49:20 +0700

    Update test_join.sql

--- test_join.sql
+++ test_join.sql
@@ -18,3 +18,51 @@ INNER JOIN currencies
 USING (code)
 WHERE region = 'North America' OR region IS NULL
 ORDER BY region;
+
+SELECT c.code AS country_code, c.name, e.year, inflation_rate
+FROM countries AS c
+INNER JOIN economies AS e
+ON c.code = e.code;
+
+SELECT c.code, c.name, c.region, p.year, p.fertility_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code;
+
+SELECT c.code, name, region, e.year, fertility_rate, e.unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+INNER JOIN economies AS e
+ON c.code = e.code;
+
+SELECT c.code, name, region, e.year, fertility_rate, unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+INNER JOIN economies AS e
+ON c.code = e.code AND p.year = e.year;
+
+SELECT p1.country_code, 
+       p1.size AS size2010,
+       p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON  p1.country_code = p2.country_code;
+
+SELECT p1.country_code, 
+       p1.size AS size2010,
+       p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code
+    AND p1.year = p2.year - 5;
+
+SELECT p1.country_code, 
+       p1.size AS size2010,
+       p2.size AS size2015,
+       ((p2.size - p1.size)/p1.size * 100.0) AS growth_perc
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code
+    AND p1.year = p2.year - 5;
