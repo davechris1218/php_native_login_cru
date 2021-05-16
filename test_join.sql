3e75909770d088f43d65cc062cd7e4515825b18b
davechris1218 <74490003+davechris1218@users.noreply.github.com>
2021-05-16 22:48:40 +0700

    test join table

--- test_join.sql
+++ test_join.sql
@@ -66,3 +66,32 @@ FROM populations AS p1
 INNER JOIN populations AS p2
 ON p1.country_code = p2.country_code
     AND p1.year = p2.year - 5;
+
+SELECT cities.name AS city, urbanarea_pop, countries.name AS country,
+       indep_year, languages.name AS language, percent
+FROM languages
+RIGHT JOIN countries
+ON countries.code = languages.code
+RIGHT JOIN cities
+ON cities.country_code = countries.code
+ORDER BY city, language;
+
+SELECT c.name AS city, l.name AS language
+FROM cities AS c        
+CROSS JOIN languages AS 1
+WHERE c.name LIKE 'Hyder%';
+
+SELECT c.name AS city, l.name AS language
+FROM cities AS c        
+INNER JOIN languages AS l
+ON c.country_code = l.code
+WHERE c.name LIKE 'Hyder%';
+
+SELECT c.name as country, region, p.life_expectancy as life_exp
+FROM countries as c
+LEFT JOIN populations as p
+ON c.code = p.country_code
+WHERE p.year = 2010
+ORDER BY life_exp
+LIMIT 5
+
