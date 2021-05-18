0d1e689ea3b323a086314ce2fa21ba54d672495b
davechris1218 <74490003+davechris1218@users.noreply.github.com>
2021-05-18 23:29:10 +0700

    native login crud

--- index_login.php
+++ index_login.php
@@ -34,140 +34,125 @@
 
 <body class="hold-transition skin-blue sidebar-mini layout-fixed">
 
-    <div class="wrapper">
+    <div class="container">
+        <!-- Modal -->
+        <div class="modal fade" id="empModal" role="dialog">
+            <div class="modal-dialog">
 
-        <header class="main-header">
-            <!-- Logo -->
-            <a href="index.php" class="logo">
-                <!-- mini logo for sidebar mini 50x50 pixels -->
-                <span class="logo-mini"><b>A</b>LT</span>
-                <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><b>Admin</b>LTE</span>
-            </a>
-            <!-- Header Navbar: style can be found in header.less -->
-            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Item Info</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
 
-            </nav>
-        </header>
-        <!-- Left side column. contains the logo and sidebar -->
-        <aside class="main-sidebar">
-            <!-- sidebar: style can be found in sidebar.less -->
-            <section class="sidebar">
-                <!-- Sidebar user panel -->
-                <div class="user-panel">
-
-                    <div class="pull-left">
-
-                        <p><strong>
-                                <font color="white"><?php echo $_SESSION['email']; ?></font>
-                            </strong></p>
-
-                        <a href="index.php?logout='1'">
-                            <font color="red">Sign Out</font>
-                        </a>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                     </div>
                 </div>
-                <!-- sidebar menu: : style can be found in sidebar.less -->
-                <ul class="sidebar-menu" data-widget="tree">
-                    <li class="header">MAIN NAVIGATION</li>
-                    <li class="active treeview">
-                        <a href="#">
-                            <i class="fa fa-dashboard"></i> <span>Menu</span>
-                            <span class="pull-right-container">
-                                <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                        </a>
-                        <ul class="treeview-menu">
-                            <li><a href="index.php"><i class="fa fa-circle-o"></i> Dashboard</a></li>
-                            <li><a href="index_2.php"><i class="fa fa-circle-o"></i> Item</a></li>
-                            <li><a href="crud.php"><i class="fa fa-circle-o"></i> Table</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </section>
-            <!-- /.sidebar -->
-        </aside>
-
-        <!-- ./wrapper -->
-
-        <body class="hold-transition skin-blue sidebar-mini layout-fixed">
-
-            <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
-                <section class="content-header">
-                    <h1>
-                        Item Showcase
-                    </h1>
-                </section>
-
-                <!-- Main content -->
-                <section class="content">
-                    <div class="row">
-                        <?php include 'table_join.php';
-                        $result = array();
-                        while ($fetch =  mysqli_fetch_assoc($query)) {
-                            $result[] = $fetch;
-                        }
-                        foreach ($result as $fetch) {
-                        ?>
-                            <div class="col-xs-3">
-                                <p><?php echo $fetch['item_image']; ?></p>
-                                <br>
-                                <p><?php echo $fetch['item_name']; ?></p>
-                                <br>
-                                <p><?php echo $fetch['item_price']; ?></p>
-                                <br>
-                                <p><?php echo $fetch['description']; ?></p>
-                                <br>
-                                <p><?php echo $fetch['item_type']; ?></p>
-                                <a><button>Click for details</button></a>
-                            </div>
-                        <?php
-                        }
-                        ?>
-                    </div>
-                    <!-- /.row -->
-                </section>
-                <!-- /.content -->
             </div>
+        </div>
 
-            <!-- jQuery 3 -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
-            <!-- jQuery UI 1.11.4 -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery-ui/jquery-ui.min.js"></script>
-            <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-            <script>
-                $.widget.bridge('uibutton', $.ui.button);
-            </script>
-            <!-- Bootstrap 3.3.7 -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-            <!-- Morris.js charts -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/raphael/raphael.min.js"></script>
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/morris.js/morris.min.js"></script>
-            <!-- Sparkline -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-            <!-- jvectormap -->
-            <script src="https://adminlte.io/themes/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-            <script src="https://adminlte.io/themes/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-            <!-- jQuery Knob Chart -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-            <!-- daterangepicker -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/moment/min/moment.min.js"></script>
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-            <!-- datepicker -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-            <!-- Bootstrap WYSIHTML5 -->
-            <script src="https://adminlte.io/themes/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-            <!-- Slimscroll -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-            <!-- FastClick -->
-            <script src="https://adminlte.io/themes/AdminLTE/bower_components/fastclick/lib/fastclick.js"></script>
-            <!-- AdminLTE App -->
-            <script src="https://adminlte.io/themes/AdminLTE/dist/js/adminlte.min.js"></script>
-            <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-            <script src="https://adminlte.io/themes/AdminLTE/dist/js/pages/dashboard.js"></script>
-            <!-- AdminLTE for demo purposes -->
-            <script src="https://adminlte.io/themes/AdminLTE/dist/js/demo.js"></script>
-        </body>
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                    Item Showcase
+                </h1>
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="row">
+                    <?php include 'table_join.php';
+                    $result = array();
+                    while ($fetch =  mysqli_fetch_assoc($query)) {
+                        $result[] = $fetch;
+                    }
+                    foreach ($result as $fetch) {
+                    ?>
+                        <div class="col-xs-3">
+                            <p><?php echo $fetch['item_image']; ?></p>
+                            <br>
+                            <p><?php echo $fetch['item_name']; ?></p>
+                            <br>
+                            <p><?php echo $fetch['item_price']; ?></p>
+                            <br>
+                            <p><?php echo $fetch['description']; ?></p>
+                            <br>
+                            <p><?php echo $fetch['item_type']; ?></p>
+                            <a><button>Click for details</button></a>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+                <!-- /.row -->
+            </section>
+            <!-- /.content -->
+        </div>
+
+        <!-- jQuery 3 -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
+        <!-- jQuery UI 1.11.4 -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery-ui/jquery-ui.min.js"></script>
+        <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+        <script>
+            $.widget.bridge('uibutton', $.ui.button);
+        </script>
+        <!-- Bootstrap 3.3.7 -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <!-- Morris.js charts -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/raphael/raphael.min.js"></script>
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/morris.js/morris.min.js"></script>
+        <!-- Sparkline -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+        <!-- jvectormap -->
+        <script src="https://adminlte.io/themes/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="https://adminlte.io/themes/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+        <!-- jQuery Knob Chart -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+        <!-- daterangepicker -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/moment/min/moment.min.js"></script>
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+        <!-- datepicker -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+        <!-- Bootstrap WYSIHTML5 -->
+        <script src="https://adminlte.io/themes/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+        <!-- Slimscroll -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+        <!-- FastClick -->
+        <script src="https://adminlte.io/themes/AdminLTE/bower_components/fastclick/lib/fastclick.js"></script>
+        <!-- AdminLTE App -->
+        <script src="https://adminlte.io/themes/AdminLTE/dist/js/adminlte.min.js"></script>
+        <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+        <script src="https://adminlte.io/themes/AdminLTE/dist/js/pages/dashboard.js"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="https://adminlte.io/themes/AdminLTE/dist/js/demo.js"></script>
+
+        <script>
+            $(document).ready(function() {
+
+                $('.userinfo').click(function() {
+
+                    var userid = $(this).data('id');
+
+                    $.ajax({
+                        url: 'ajaxfile.php',
+                        type: 'post',
+                        data: {
+                            userid: userid
+                        },
+                        success: function(response) {
+                            $('.modal-body').html(response);
+                            $('#empModal').modal('show');
+                        }
+                    });
+                });
+            });
+        </script>
+</body>
 
 </html>
