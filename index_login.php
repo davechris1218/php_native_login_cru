4d7a6af6ee0751a01a00c80d3b3361df811a6027
davechris1218 <74490003+davechris1218@users.noreply.github.com>
2021-05-29 22:49:15 +0700

    native login crud

--- index_login.php
+++ index_login.php
@@ -76,7 +76,6 @@
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                             </div>
                             <div class="modal-body">
-
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
